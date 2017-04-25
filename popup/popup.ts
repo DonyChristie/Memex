0aeeb223dd8bfb39fc995a1c777f1a8889066e4f
Seth Hovestol <Seth.Hovestol@colorado.edu>
2017-04-25 14:47:48 -0600

    Switched to typescript and got the most basic start working

---
+++ popup/popup.ts
@@ -0,0 +1,22 @@
+function ToggleRecording(rec) {
+  console.log(rec);
+  var button = document.getElementById("recordButton");
+  console.log(button);
+  button.addEventListener('click',function tempFunc(){
+    button.removeEventListener('click',tempFunc);
+    ToggleRecording(!rec);
+  });
+  button.innerHTML = ((rec)? "Start":"Stop") + " Recording";
+}
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var record = document.getElementById("recordButton");
+  record.addEventListener('click',function tempFunc(){
+    record.removeEventListener('click', tempFunc);
+    ToggleRecording(true);
+  });
+});
