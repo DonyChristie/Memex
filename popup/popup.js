a52707865e89419c949887068276a0c3c6623334
Seth Hovestol <Seth.Hovestol@colorado.edu>
2017-05-06 08:17:11 -0600

    Getting closer to a solution. Need to implement storage for connections next and get my selection scheme working

--- popup/popup.js
+++ popup/popup.js
@@ -1,43 +1,93 @@
-function ToggleRecording(rec) {
-    var button = document.getElementById("recordButton");
-    console.log(button);
-    button.addEventListener('click', function tempFunc() {
-        button.removeEventListener('click', tempFunc);
-        ToggleRecording(!rec);
-    });
-    button.innerHTML = ((rec) ? "Stop" : "Start") + " Recording";
-}
-var rec = true;
-
-
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("recordButton").addEventListener('click',function(){onRecordClick();});
-    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-        var url = tabs[0].url;
-        chrome.storage.local.get(url, function (items){
-            var note = items['note'];
-            note = (note === undefined)? "Write a note here!":note;
-            var noteArea = document.getElementById("notes");
-            noteArea.innerHTML = note;
-        });
+  var record = document.getElementById("recordButton");
+  record.addEventListener('click',onRecordClick);
+  record.innerHTML = "Show Link Maker";
+  
+  var select = document.getElementById("linkSelect");
+  select.addEventListener('click',multiselect);
+  
+  var noteArea = document.getElementById("notes");
+  noteArea.addEventListener("input",function(vars){
+    var val = vars.srcElement.value;
+    chrome.tabs.query({'active':true, 'lastFocusedWindow':true}, doMemFuncUrl(updateMemory,val));
+  });
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, doMemFuncUrl(loadFromMemory));
+});
+
+function updateLinkMaker() {
+  var item = document.getElementById("linkSelect");
+  item.innerHTML = "";
+  chrome.tabs.query({},function (results){
+    for(i in results){
+      var option = document.createElement("option");
+      option.text = results[i].title;
+      option.value = results[i].url;
+      item.add(option);
+    };
+  });
+  
+  var oldConns = document.getElementById("connectionList");
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (results){
+    chrome.storage.local.get("connections",function (items) {
+      connections = items[results[0].url];
+      connections = (connections === undefined)? []: connections;
+      for(i in connections){
+        var option = document.createElement("option");
+        option.text = results[i];
+        option.value = results[i];
+        oldConns.add(option);
+      }
     });
+  });
+}
+
+
+function multiselect(e){
+    var el = e.target;
+    e.preventDefault();
+    // toggle selection
+    if (el.hasAttribute('selected')) el.removeAttribute('selected');
+    else el.setAttribute('selected', '');
+
+    // hack to correct buggy behavior
+    var select = el.parentNode.cloneNode(true);
+    el.parentNode.parentNode.replaceChild(select, el.parentNode);
+
+}
+
+function onRecordClick(){
+  var record = document.getElementById("recordButton")
+  var maker = document.getElementById("recordBox");
+  if(record.innerHTML == "Show Link Maker"){
+    updateLinkMaker();
+    record.innerHTML = "Hide Link Maker";
+    maker.style.display = "block";
+  } else {
+    record.innerHTML = "Show Link Maker";
+    maker.style.display = "none";
+  }
+};
+
+function updateMemory(url,other) {
+  var val = other[0];
+  return function(items) {
+    items[url]['note'] = val;
+    chrome.storage.local.set(items,function () {console.log(tmp);});
+  };
+};
+
+function loadFromMemory(url,other) {
+  return function(items) {
+    var note = items[url]['note'];
+    note = (note === undefined)? "Write a note here!":note;
     var noteArea = document.getElementById("notes");
-    console.log(noteArea);
-    noteArea.addEventListener("onkeyup",function(vars){
-        console.log(vars);
-        console.log("hello!");
-    });
-});
+    noteArea.innerHTML = note;
+  };
+};
 
-
-/*
-chrome.tabs.query({'active':true, 'lastFocusedWindow':true}, function (tabs) {
+function doMemFuncUrl(func,other){
+  return function(tabs){
     var url = tabs[0].url;
-    chrome.storage.local.get(url, function (items){
-        items['note'];
-        note = (note === undefined)? "Write a note here!":note;
-        var noteArea = document.getElementById("notes");
-        noteArea.innerHTML = note;
-    });
-});
-*/
+    chrome.storage.local.get(url, func(url,other));
+  };
+};
